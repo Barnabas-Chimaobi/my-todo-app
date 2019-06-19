b98ba6475e6323e106bc8610acabc4d10da6d8fd
Barnabas <Barnabas-chimaobi>
2019-06-19 12:11:26 +0100

    updated todo app

---
+++ src/TodoItems.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+const TodoItems = ()=> {
+  return (
+    <h2>I render individual items</h2>
+  )
+}
+export default TodoItems;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Todo from './Todo'
+import TodoItems from './TodoItems'
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
